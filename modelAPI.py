51375498f14150167da29d114d9e2c74e3648614
Laurent Géran <lgeran@dial-once.com>
2022-06-21 22:47:25 +0200

    modelAPI

--- modelAPI.py
+++ modelAPI.py
@@ -25,6 +25,7 @@ def predict_score(data:creditApp.credit_application):
 
 # run the api
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=4000, debug=True)
+    uvicorn.run('modelAPI:app' , host='127.0.0.1', port=4000, debug=True)
+#uvicorn  modelAPI:app --reload
 
 
