c945e14953548951799ea2a0bf3e799650fc1361
Laurent Géran <lgeran@dial-once.com>
2022-08-01 16:32:21 +0200

    final commit

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import plot
 import pandas as pd
 import numpy as np
 import sqlite3
@@ -7,6 +8,8 @@ import warnings
 
 # Modeling
 from lightgbm import LGBMClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.svm import LinearSVC
 
 # Hyperparameters 
 from skopt.utils import use_named_args
@@ -23,6 +26,7 @@ from hyperopt import hp
 from hyperopt import Trials
 from hyperopt import fmin
 from hyperopt import tpe
+from hyperopt import STATUS_OK
 
 
 # Serialization
@@ -44,11 +48,18 @@ applicationTrain = preproFunc.localLoad('applicationTrain', con, True)
 applicationTrain_y= np.ravel(preproFunc.localLoad('applicationTrain_y', con, True))
 applicationTrain_X= preproFunc.localLoad('applicationTrain_X', con, True).drop(columns=["SK_ID_CURR"])
 
+print("NO SAMPLING :")
 model = modelFunc.modeling(LGBMClassifier(), applicationTrain_X, applicationTrain_y)
+model_sgd = modelFunc.modeling(SGDClassifier(loss='modified_huber'), applicationTrain_X, applicationTrain_y)
 
 # sampling
+print("UNDERSAMPLING :")
 model_under = modelFunc.modeling(LGBMClassifier(), applicationTrain_X, applicationTrain_y, sampling = -1)
+model_under_sgd = modelFunc.modeling(SGDClassifier(loss='modified_huber'), applicationTrain_X, applicationTrain_y, sampling = -1)
+
+print("OVERSAMPLING :")
 model_over = modelFunc.modeling(LGBMClassifier(), applicationTrain_X, applicationTrain_y, sampling = 1)
+model_over_sgd = modelFunc.modeling(SGDClassifier(loss='modified_huber'), applicationTrain_X, applicationTrain_y, sampling = 1)
 
 # feature selection with BorutaPy
 feat_selector = BorutaPy(LGBMClassifier(num_boost_round=100), n_estimators='auto', random_state=1)
@@ -70,40 +81,63 @@ featSelect.to_sql('featSelect', con)
 featSelect_y.to_sql('featSelect_y', con)
 featSelect_X.to_sql('featSelect_X', con)
 
-model = modelFunc.modeling(LGBMClassifier(), featSelect_X, applicationTrain_y, sampling = 0)
-model_under = modelFunc.modeling(LGBMClassifier(), featSelect_X, applicationTrain_y, sampling = -1)
-model_over = modelFunc.modeling(LGBMClassifier(), featSelect_X, applicationTrain_y, sampling = 1)
+model_under = modelFunc.modeling(LGBMClassifier(), featSelect_X, featSelect_y, sampling = -1)
 
 # optimisation des hyperparamètres
 space = {
     'n_estimators': hp.quniform('n_estimators', 200, 800, 200),
-    #'class_weight': hp.choice('class_weight', [None, 'balanced']),
+    'class_weight': hp.choice('class_weight', [None, 'balanced']),
     'max_depth' : hp.quniform('max_depth', 2, 30, 2),
     'learning_rate': hp.loguniform('learning_rate', np.log(0.005), np.log(0.2)),
     'subsample': hp.quniform('subsample', 0.1, 1.0, 0.2),
-    #'colsample_bytree': hp.quniform('colsample_by_tree', 0.6, 1.0, 0.1),
     'num_leaves': hp.quniform('num_leaves', 4, 100, 4),
     'reg_alpha': hp.quniform('reg_alpha', 0.1, 1.0, 0.1),
     'reg_lambda': hp.quniform('reg_lambda', 0.1, 1.0, 0.1),
-    #'solvability_threshold': hp.quniform('solvability_threshold', 0.0, 1.0, 0.025)
+    'solvability_threshold': hp.quniform('solvability_threshold', 0.0, 1.0, 0.025)
 }
 
+def f(params):
+    best = 1
+    loss = modelFunc.hyperopt_train_test(params, featSelect_X, featSelect_y)
+    if loss < best:
+        best = loss
+        print ('new best:', best, params)
+    return {'loss': loss, 'status': STATUS_OK}
+
 trials = Trials()
-best = fmin(modelFunc.f, space, algo=tpe.suggest, max_evals=5, trials=trials)
+best = fmin(f, space, algo=tpe.suggest, max_evals=5, trials=trials)
 
 print('best:')
+
+if best['class_weight']==1:
+    best['class_weight']='balanced'
+else:
+    best['class_weight']=None
+
 print(best)
 
-params = {'learning_rate': 0.06830366370859095, 'max_depth': 22, 'n_estimators': 400, 'num_leaves': 24, 'reg_alpha': 0.4, 'reg_lambda': 0.7000000000000001, 'subsample': 0.6000000000000001}
+paramsFinal = {
+    'n_estimators': int(best['n_estimators']),
+    'class_weight': best['class_weight'],
+    'max_depth': int(best['max_depth']), 
+    'learning_rate': best['learning_rate'],
+    'subsample': best['subsample'],
+    'num_leaves': int(best['num_leaves']),
+    'reg_alpha': best['reg_alpha'],
+    'reg_lambda': best['reg_lambda']
+}
+threshFinal = best['solvability_threshold']
 
-model = modelFunc.modeling(LGBMClassifier(**params), applicationTrain_X, applicationTrain_y)
+modelFinal = modelFunc.modeling(LGBMClassifier(**paramsFinal), featSelect_X, featSelect_y, sampling = -1, thresh = threshFinal)
 
-y_pred = model.predict(applicationTrain_X)
+#y_pred = modelFinal[0].predict(featSelect_X)
 
-confusion_matrix(applicationTrain_y,y_pred)
+#print(confusion_matrix(featSelect_y,y_pred))
 
 # saving the model
 with open('homecredit_model/clf_feat_under.pkl', 'wb') as output_file:
-    pickle.dump(model_under[0], output_file)
+    pickle.dump(modelFinal[0], output_file)
 
 con.close()
+
+#{'class_weight': 'balanced', 'learning_rate': 0.04322399776800654, 'max_depth': 28.0, 'n_estimators': 400.0, 'num_leaves': 52.0, 'reg_alpha': 0.4, 'reg_lambda': 0.30000000000000004, 'solvability_threshold': 0.35000000000000003, 'subsample': 1.0}
--- modelFunc.py
+++ modelFunc.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Modeling 
 from lightgbm import LGBMClassifier
@@ -13,10 +14,13 @@ from imblearn.under_sampling import RandomUnderSampler
 
 # Metrics
 from sklearn.metrics import make_scorer
-from sklearn.metrics import roc_auc_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import brier_score_loss
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import auc
 
 # Model selection
 from sklearn.model_selection import train_test_split
@@ -28,7 +32,7 @@ from sklearn.model_selection import cross_val_predict
 from hyperopt import STATUS_OK
 
 
-def modeling(classifier, X, y, sampling = 0):
+def modeling(classifier, X, y, sampling = 0, thresh = 0.5, plotPR = False):
     XTrain, XTest, yTrain, yTest = train_test_split(X,y, test_size=0.2)
     transformer = RobustScaler()
     XTrain = transformer.fit_transform(XTrain)
@@ -41,17 +45,51 @@ def modeling(classifier, X, y, sampling = 0):
         XTrain, yTrain = randomundersampling.fit_resample(XTrain, yTrain)
     else : 
         pass
-    auc = make_scorer(roc_auc_score)
+    auc_score = make_scorer(roc_auc_score)
     accuracy = make_scorer(accuracy_score)
     g_norm_score = make_scorer(customMetric)
-    scoring = {'auc': auc, 'accuracy': accuracy, 'gain':g_norm_score}
+    scoring = {'auc': auc_score, 'accuracy': accuracy, 'gain':g_norm_score}
     cv = cross_validate(classifier,XTrain, yTrain , cv=4,scoring = scoring)
     print('score auc : {score}'.format(score=np.mean(cv['test_auc'])))
     print('score accuracy : {score}'.format(score=np.mean(cv['test_accuracy'])))
-    print('score gain : {score}'.format(score=np.mean(cv['test_gain'])))
+    print('score gain train: {score}'.format(score=np.mean(cv['test_gain'])))
+
     model = classifier.fit(XTrain, yTrain)
-    yPred = classifier.predict(XTest)
-    print('score gain : {score}'.format(score=g_norm_score(model,XTest,yTest)))
+    yPred_proba = classifier.predict_proba(XTest)
+    yPred=[1 if x[1]>thresh else 0 for x in yPred_proba]
+    yPred_proba_positive = yPred_proba[:, 1]
+    
+    # calculate inputs for the PR curve
+    precision, recall, thresholds = precision_recall_curve(yTest, yPred_proba_positive)
+
+    print(precision)
+    print(recall)
+
+    if plotPR :
+        # calculate the no skill line as the proportion of the positive class
+        no_skill = len(y[y==1]) / len(y)
+        # plot the no skill precision-recall curve
+        plt.plot([0, 1], [no_skill, no_skill], linestyle='--', label='No Skill')
+        # plot PR curve
+        plt.plot(recall, precision, marker='.', label='classifier')
+        # axis labels
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        # show the legend
+        plt.legend()
+        #plt.show(block=False)
+        plt.show()
+
+    # calculate and print PR AUC
+    auc_pr = auc(recall, precision)
+
+    # calculate and print Brier Score
+    brier = brier_score_loss(yTest, yPred_proba_positive)
+    
+    
+    print('score gain test : {score}'.format(score=g_norm_score(model,XTest,yTest)))
+    print('AUC PR: %.3f' % auc_pr)
+    print('Brier Score: %.3f' % (brier))
     print('classification report : ', classification_report(yTest,yPred))
 
     return model, XTrain, yTrain
@@ -72,7 +110,7 @@ def customMetric(y, y_pred, fn_value=-10, fp_value=-1, tp_value=10, tn_value=1):
 
     return g_norm
 
-def hyperopt_train_test(params, applicationTrain_X, applicationTrain_y):
+def hyperopt_train_test(params, X, y):
     
     # On s'assure que les paramètres soient au bon format
     for parameter_name in ['num_leaves','max_depth','n_estimators']:
@@ -84,61 +122,26 @@ def hyperopt_train_test(params, applicationTrain_X, applicationTrain_y):
                     'max_depth': int(params['max_depth']), 
                     'learning_rate': params['learning_rate'],
                     'subsample': params['subsample'],
-                    'colsample_bytree': params['colsample_bytree'],
                     'num_leaves': int(params['num_leaves']),
                     'reg_alpha': params['reg_alpha'],
                     'reg_lambda': params['reg_lambda']
                    }
-
-    X_ = applicationTrain_X[:]
+    thresh = params['solvability_threshold']
     
-    skf = StratifiedKFold(n_splits=3)
+    skf = StratifiedKFold(n_splits=4)
     
     clf = LGBMClassifier(**params_model)
+
+    g_norm_skf = []
+
+    for train_index, test_index in skf.split(X, y):
+        clf.fit(X.loc[train_index],y[train_index])
+        proba = clf.predict_proba(X.loc[test_index])
+        y_pred=[1 if x[1]>thresh else 0 for x in proba]
+        g_norm_skf.append(customMetric(y[test_index], y_pred))
+        
+    g_norm = np.mean(g_norm_skf)
     
-    y_pred = cross_val_predict(clf, X_, applicationTrain_y, method='predict', cv=skf)
-    
-    score = customMetric(applicationTrain_y, y_pred)
-    
-    loss = 1 - score
+    loss = 1 - g_norm
     
     return loss
-
-"""space = {
-    'n_estimators': hp.quniform('n_estimators', 200, 800, 200),
-    #'class_weight': hp.choice('class_weight', [None, 'balanced']),
-    'max_depth' : hp.quniform('max_depth', 2, 30, 2),
-    'learning_rate': hp.loguniform('learning_rate', np.log(0.005), np.log(0.2)),
-    'subsample': hp.quniform('subsample', 0.1, 1.0, 0.2),
-    #'colsample_bytree': hp.quniform('colsample_by_tree', 0.6, 1.0, 0.1),
-    'num_leaves': hp.quniform('num_leaves', 4, 100, 4),
-    'reg_alpha': hp.quniform('reg_alpha', 0.1, 1.0, 0.1),
-    'reg_lambda': hp.quniform('reg_lambda', 0.1, 1.0, 0.1),
-    #'solvability_threshold': hp.quniform('solvability_threshold', 0.0, 1.0, 0.025)
-}"""
-
-def f(params):
-    best = 1
-    loss = hyperopt_train_test(params)
-    if loss < best:
-        best = loss
-        print ('new best:', best, params)
-    return {'loss': loss, 'status': STATUS_OK}
-
-"""
-trials = Trials()
-
-
-best = fmin(f, space, algo=tpe.suggest, max_evals=5, trials=trials)
-
-print('best:')
-print(best)
-
-params = {'learning_rate': 0.06830366370859095, 'max_depth': 22, 'n_estimators': 400, 'num_leaves': 24, 'reg_alpha': 0.4, 'reg_lambda': 0.7000000000000001, 'subsample': 0.6000000000000001}
-
-model = modeling(LGBMClassifier(**params), applicationTrain_X, applicationTrain_y)
-
-y_pred = model.predict(applicationTrain_X)
-
-confusion_matrix(applicationTrain_y,y_pred)
-"""
